79ad168a20b62b89afae5a9fa09ae07a111beb99
gleb <gleby-hleby@inbox.ru>
2020-02-11 13:25:06 +0300

    Scripted auto-commit on change (2020-02-11 13:25:06) by gitwatch.sh

--- src/main/java/Launcher.java
+++ src/main/java/Launcher.java
@@ -1,6 +1,11 @@
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+
 public class Launcher {
     public static void main(String[] args){
-
+        ActorSystem sys = ActorSystem.create("noname");
+        ActorRef actor = sys.actorOf(Props.create(Actor.class));
     }
 }
 
