d8da56b08754e1c50cf56661dafdcb6fbb7f766e
gleb <gleby-hleby@inbox.ru>
2020-02-11 14:42:46 +0300

    Scripted auto-commit on change (2020-02-11 14:42:46) by gitwatch.sh

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,8 +1,15 @@
 
+import akka.actor.ActorRef;
+import akka.http.javadsl.Http;
 import akka.http.javadsl.server.AllDirectives;
 
 
 public class Server extends AllDirectives {
+    private Http http;
+    private ActorRef actorRef;
 
+    public server(final Http http){
+
+    }
 
 }
