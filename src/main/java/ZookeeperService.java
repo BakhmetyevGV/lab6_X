b8ba1fd84f950a43afc0d9357c9f336bc36697b7
gleb <gleby-hleby@inbox.ru>
2020-02-14 12:50:56 +0300

    Scripted auto-commit on change (2020-02-14 12:50:56) by gitwatch.sh

--- src/main/java/ZookeeperService.java
+++ src/main/java/ZookeeperService.java
@@ -1,8 +1,11 @@
+import akka.actor.ActorRef;
+import org.apache.zookeeper.ZooKeeper;
 
 public class ZookeeperService {
     private static final String ZOOKEEPER_CONNECT_STRING = "127.0.0.1:2181";
     private static final String ROOT_PATH = "/servers";
     private static final String NODES_PATH = "/servers/s";
 
-    
+    private ZooKeeper zooKeeper;
+    private ActorRef actorRef;
 }
