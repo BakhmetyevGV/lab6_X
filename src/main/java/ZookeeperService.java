30b9c559c25cb8434ca51d742cde392442fe5550
gleb <gleby-hleby@inbox.ru>
2020-02-14 12:50:17 +0300

    Scripted auto-commit on change (2020-02-14 12:50:17) by gitwatch.sh

--- src/main/java/ZookeeperService.java
+++ src/main/java/ZookeeperService.java
@@ -3,4 +3,6 @@ public class ZookeeperService {
     private static final String ZOOKEEPER_CONNECT_STRING = "127.0.0.1:2181";
     private static final String ROOT_PATH = "/servers";
     private static final String NODES_PATH = "/servers/s";
+
+    
 }
