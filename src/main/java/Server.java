ef2860f2ebc1bbb226d1f1dbe3ee41594a8c9d52
gleb <gleby-hleby@inbox.ru>
2020-02-17 19:17:42 +0300

    Scripted auto-commit on change (2020-02-17 19:17:42) by gitwatch.sh

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -2,14 +2,18 @@
 import akka.actor.ActorRef;
 import akka.http.javadsl.Http;
 import akka.http.javadsl.server.AllDirectives;
+import org.apache.zookeeper.KeeperException;
+
+import java.io.IOException;
 
 
 public class Server extends AllDirectives {
     private Http http;
     private ActorRef actorRef;
 
-    public Server(final Http http){
-
+    public Server(final Http http, int port, ActorRef actorRef) throws InterruptedException, IOException, KeeperException {
+        this.http = http;
+        this.actorRef = actorRef;
     }
 
 }
