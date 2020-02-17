fd4a41557df7a82208e60240bd2a5ff909d83e47
gleb <gleby-hleby@inbox.ru>
2020-02-17 22:29:26 +0300

    Scripted auto-commit on change (2020-02-17 22:29:26) by gitwatch.sh

--- src/main/java/Launcher.java
+++ src/main/java/Launcher.java
@@ -37,7 +37,7 @@ public class Launcher {
         }
 
         // Создание нового узла
-        String znodePath = "/zookeepernode2";
+        String znodePath = "/zookeepernode1";
         List<ACL> acls = ZooDefs.Ids.OPEN_ACL_UNSAFE;
         if (zooKeeper.exists(znodePath, false) == null) {
             zooKeeper.create(znodePath, "data".getBytes(), acls, CreateMode.PERSISTENT);
