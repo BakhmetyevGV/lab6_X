5c50e7ba7c3ec6db26a00c62e90b215c2b11f9af
gleb <gleby-hleby@inbox.ru>
2020-02-17 22:09:42 +0300

    Scripted auto-commit on change (2020-02-17 22:09:42) by gitwatch.sh

--- src/main/java/Launcher.java
+++ src/main/java/Launcher.java
@@ -2,6 +2,8 @@ import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
 import akka.http.javadsl.Http;
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
 
 public class Launcher {
     public static void main(String[] args){
@@ -12,6 +14,17 @@ public class Launcher {
 
         String server = "127.0.0.1:2181";
         Object lock = new Object();
+
+        Watcher connectionWatcher = new Watcher() {
+            public void process(WatchedEvent we) {
+                if (we.getState() == Event.KeeperState.SyncConnected) {
+                    System.out.println("Connected to Zookeeper in " + Thread.currentThread().getName());
+                    synchronized (lock) {
+                        lock.notifyAll();
+                    }
+                }
+            }
+        };
 
     }
 }
