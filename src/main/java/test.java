45b97e480e64399e7c95e440df400e918e84d3ee
gleb <gleby-hleby@inbox.ru>
2020-02-19 00:29:19 +0300

    Scripted auto-commit on change (2020-02-19 00:29:19) by gitwatch.sh

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.ZooKeeper;
+
+import java.io.IOException;
 
 public class test {
+    private static final String ZOOKEEPER_SERVER = "127.0.0.1:2181";
+    private static final int SESSION_TIMEOUT = 5000;
+
+    public static void main(String[] args) throws IOException, KeeperException, InterruptedException {
+        ZooKeeper zk = new ZooKeeper(ZOOKEEPER_SERVER, SESSION_TIMEOUT, null);
+
+        for(String node : zk.getChildren("/", null)){
+            System.out.println(node);
+        }
+    }
+
 }
