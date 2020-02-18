0f0233fc712957b6ae4eab052212b3c4466a875a
gleb <gleby-hleby@inbox.ru>
2020-02-18 20:16:19 +0300

    Scripted auto-commit on change (2020-02-18 20:16:19) by gitwatch.sh

--- src/main/java/HttpActor.java
+++ src/main/java/HttpActor.java
@@ -1,2 +1,11 @@
-public class HttpActor {
+import akka.actor.AbstractActor;
+import akka.japi.pf.ReceiveBuilder;
+import java.util.Random;
+
+public class HttpActor extends AbstractActor {
+
+    @Override
+    public Receive createReceive() {
+        return null;
+    }
 }
